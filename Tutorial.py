4cdd504d4d4d44393a6d3b12e5d04cca1ecc1455
MrMattheuu <mattboylan07@gmail.com>
2023-02-09 10:36:04 -0700

    added comments

---
+++ Tutorial.py
@@ -0,0 +1,14 @@
+'''
+@Matthew Boylan
+Thursday Feb. 9th
+writen to serve as a tutorial...
+'''
+#imports
+#comment describing purpose/where import used
+
+#variables & CONSTANTS( :) )
+#comment what they're for ^^^^
+
+#def ALL functions.
+# include comments describing fucntion task
+#"program code that runs function calls"?
