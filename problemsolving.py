9025881f72dc22451176928222009be9169d7e8c
MrMattheuu <mattboylan07@gmail.com>
2023-02-15 10:37:53 -0700

    classtime work. Add dedection if it is just spaces

---
+++ problemsolving.py
@@ -0,0 +1,40 @@
+#====================================
+#Author:                            |
+#Matthew Boylan                     |
+#                                   |
+#File Name:                         |
+#Problemsolving.py                  |
+#                                   |
+#Objective:                         |
+#I am going to be solving problems  |
+#                                   |
+#date:                              |
+#Feb 15 2023                        |
+#====================================
+
+#357 until my birthday!!!
+#3 days until Trebbe's brithday!!!
+
+#onansnumbers = "Ronan loves to count numbers!!!"
+
+#print(ronansnumbers.count("!"))
+
+#print(ronansnumbers.split("o"))
+
+
+#GET INPUT
+input = input("Please type stuff here: ")
+
+#GET THE AMOUNT OF WORDS
+if input.count("  ") >= 0:
+    #dont count the spaces
+    *placeholder*
+
+if input.count("  ") == 0:
+    #count spaces
+    wordcount = (input.count("x "))
+#wordcount=(input.count(" ")) + 1
+
+#PRINT WORD COUNT
+print(f"the word count of the message is: {wordcount}")
+
